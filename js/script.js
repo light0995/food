f1ef4d33e5d8f128ed254e901cbb689298f09162
Akhmat <light95@list.ru>
2025-05-17 15:35:34 +0800

    last commit

--- js/script.js
+++ js/script.js
@@ -228,9 +228,8 @@ function changePostData (form) {
       closeModal();
     }, 3000);
   }
-
-
 };
+
 
 forms.forEach(form => changePostData(form));
 getData('http://localhost:3000/menu')
@@ -455,6 +454,35 @@ const modalTimerId = setTimeout(openModal, 3000);
 setClock(".promotion__timer", deadline);
 switchPreviewMeals();
 hamburgerToggle();
+
+
+
+
+
+
+//Calc 
+
+const result = document.querySelector('.calculating__result span');
+let sex, height, weight, age, ratio; 
+
+
+function calcTotal () {
+  if (!sex || !height || !weight || !age || !ratio) {
+    result.textContent = `_____`;
+    return;
+  }
+  
+  if (sex === `female`) {
+    result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+  } else {
+    result.textContent = (88.36  + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+  }
+};
+
+
+calcTotal();
+
+
 
 
 
